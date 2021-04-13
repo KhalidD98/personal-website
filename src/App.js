2f18172dc76fcd6d3b0b1b39a3d07a1d698e2266
Khalid Dakak <khalid.dakak8@gmail.com>
2021-04-13 14:51:47 -0500

    Created main page container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import './App.css';
+import LeftSide from './LeftSide';
+import { Grid } from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core/styles';
+import RightSide from './RightSide';
+
+const useStyles = makeStyles({
+  left_side: {
+
+  },
+  right_side: {
+    position: 'static',
+    overflow: 'visible',
+    backgroundColor: 'red',
+  },
+  main: {
+    minHeight: '100%'
+  }
+})
 
 function App() {
+  const classes = useStyles()
   return (
     <>
-      <h1>This is a test</h1>
+      <Grid container direction="row"
+        justify="center"
+        alignItems="center"
+        className={classes.main}>
+        <Grid item xs={5} className={classes.left_side}>
+          <LeftSide />
+        </Grid>
+        <Grid item xs={7} className={classes.right_side}>
+          <RightSide />
+        </Grid>
+      </Grid>
     </>
   );
 }
---
+++ src/LeftSide.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    name: {
+
+    }
+})
+
+export default function LeftSide() {
+    const classes = useStyles()
+    return (
+        
+    )
+}
