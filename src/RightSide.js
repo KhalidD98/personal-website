c574e5882d1d8091d3ddc5a7fb9b968e855e0fe9
Khalid Dakak <khalid.dakak8@gmail.com>
2021-04-13 20:06:19 -0500

    Process of making first project card

--- src/RightSide.js
+++ src/RightSide.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
+import { Card } from '@material-ui/core';
+import CardHeader from '@material-ui/core/CardHeader';
 
 const useStyles = makeStyles({
     container: {
         height: '100%'
     },
     project_one: {
-        width: '100%'
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'red'
     },
     project_two: {
 
@@ -17,6 +21,9 @@ const useStyles = makeStyles({
     },
     project_four: {
 
+    },
+    project_one_card: {
+        backgroundColor: 'red'
     }
 })
 
@@ -30,7 +37,9 @@ export default function RightSide() {
                 alignItems="center"
                 className={classes.container}>
                 <Grid item className={classes.project_one}>
+                    <Card className={classes.project_one_card}>
 
+                    </Card>
                 </Grid>
                 <Grid item className={classes.project_two}>
 
