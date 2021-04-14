d72d451a30ba71f11169f43d84d78ee01ef2a70d
Khalid Dakak <khalid.dakak8@gmail.com>
2021-04-14 18:21:33 -0500

    Added icons, need to make dynamic

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,10 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles'
+import { DiReact, DiJsBadge, DiCss3 } from "react-icons/di"
+import { SiMaterialUi } from "react-icons/si";
+import { Grid } from '@material-ui/core';
+import projectData from './content/projects.json'
+
 
 const useStyles = makeStyles((theme) => ({
     main: {
@@ -16,7 +21,20 @@ const useStyles = makeStyles((theme) => ({
         padding: '0',
         margin: '0',
         marginTop: '17%',
-        marginBottom: '8%'
+        marginBottom: '7%',
+        fontFamily: 'poppins'
+    },
+    description: {
+        width: '65%',
+        fontFamily: 'poppins',
+        textAlign: 'center',
+        marginBottom: '10%'
+    },
+    icons: {
+        width: '60px',
+        height: '60px'
+    },
+    iconContainer: {
     }
 }))
 
@@ -25,7 +43,36 @@ export default function Projects({ data }) {
     return (
         <div className={classes.main}>
             <h1 className={classes.title}>{data.name}</h1>
-            <h2>{data.description}</h2>
+            <h2 className={classes.description}>{data.description}</h2>
+            <Grid container
+                direction="row"
+                justify="center"
+                alignItems="center"
+                className={classes.iconContainer}
+                spacing={5}>
+
+                {projectData[0].icons.forEach((icon) => {
+                    console.log(icon)
+                    return (
+                        <Grid item>
+                            <DiReact className={classes.icons} />
+                        </Grid>
+                    )
+                })}
+
+                {/*<Grid item>
+                    <DiReact className={classes.icons} />
+                </Grid>
+                 <Grid item>
+                    <DiJsBadge className={classes.icons} />
+                </Grid>
+                <Grid item>
+                    <DiCss3 className={classes.icons} />
+                </Grid>
+                <Grid item>
+                    <SiMaterialUi className={classes.icons} />
+                </Grid> */}
+            </Grid>
         </div>
     )
 }
