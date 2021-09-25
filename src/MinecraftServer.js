968291a20385d260c32eb26df945268a2b25acde
Khalid Dakak <khalid.dakak8@gmail.com>
2021-09-25 16:17:56 -0500

    Added curves

--- src/MinecraftServer.js
+++ src/MinecraftServer.js
@@ -24,8 +24,8 @@ const useStyles = makeStyles((theme) => ({
         overflow: 'hidden'
     },
     title: {
-        marginBottom: '10vh',
-        marginTop: '20vh',
+        marginBottom: '4vh',
+        marginTop: '8vh',
         fontSize: '7vh',
         textAlign: 'center',
         margin: '10px'
@@ -34,7 +34,7 @@ const useStyles = makeStyles((theme) => ({
         width: '74vw',
         fontSize: '26px',
         textAlign: 'center',
-        marginBottom: '170px',
+        marginBottom: '70px',
         marginTop: '40px'
     },
     homeButton: {
@@ -46,14 +46,10 @@ const useStyles = makeStyles((theme) => ({
         marginLeft: '50%'
     },
     videoContainer: {
-        // margin: '10px',
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         alignItems: 'center',
-        height: '600px',
-        margin: '10px',
-        paddingTop: '10px'
     },
     exampleText: {
         width: '90%',
@@ -118,7 +114,9 @@ const useStyles = makeStyles((theme) => ({
     },
     darkBackground: {
         width: '100%',
-        background: '#FF6960',
+        background: '#ff4f47',
+        padding: '0',
+        margin: '0'
     },
     coloredAchievementText: {
         color: '#2827CC'
@@ -152,6 +150,9 @@ const useStyles = makeStyles((theme) => ({
         color: 'white'
     },
     featuresContainer: {
+        width: '100%'
+    },
+    featuresContainerDiv: {
         display: 'block',
         marginLeft: 'auto',
         marginRight: 'auto',
@@ -168,6 +169,7 @@ const useStyles = makeStyles((theme) => ({
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
+        width: '100%'
     },
     examplesContainer: {
         width: '100%',
@@ -194,6 +196,78 @@ const useStyles = makeStyles((theme) => ({
         },
     },
 
+    achievementCurve: {
+        top: '0',
+        left: '0',
+        width: '100%',
+        overflow: 'hidden',
+        lineHeight: '0',
+        marginBottom: '6vh'
+    },
+    achievementCurveSVG: {
+        position: 'relative',
+        display: 'block',
+        width: 'calc(104% + 1.3px)',
+        height: '60px',
+    },
+    curveFillGray: {
+        fill: '#393E46',
+    },
+    curveFillRed: {
+        fill: '#ff4f47',
+    },
+    curveFillBlue: {
+        fill: '#008698',
+    },
+    achievementCurveBottom: {
+        top: '0',
+        left: '0',
+        width: '100%',
+        overflow: 'hidden',
+        lineHeight: '0',
+        transform: 'rotate(180deg)',
+        marginBottom: '-1px',
+        marginTop: '4vh'
+    },
+    achievementCurveSVGBottom: {
+        position: 'relative',
+        display: 'block',
+        width: 'calc(104% + 1.3px)',
+        height: '80px',
+    },
+    examplesCurveBottom: {
+        top: '0',
+        left: '0',
+        width: '100%',
+        overflow: 'hidden',
+        lineHeight: '0',
+        transform: 'rotate(180deg)',
+        marginBottom: '-1px',
+        marginTop: '4vh'
+    },
+    examplesCurveSVGBottom: {
+        position: 'relative',
+        display: 'block',
+        width: 'calc(144% + 1.3px)',
+        height: '80px',
+    },
+    footerCurve: {
+        top: '0',
+        left: '0',
+        width: '100%',
+        overflow: 'hidden',
+        lineHeight: '0',
+        transform: 'rotate(180deg)',
+        backgroundColor: '#393E46'
+    },
+    footerCurveSVG: {
+        position: 'relative',
+        display: 'block',
+        width: 'calc(144% + 1.3px)',
+        height: '80px',
+    }
+
+
 }))
 
 export default function MinecraftServer({ data }) {
@@ -215,8 +289,7 @@ export default function MinecraftServer({ data }) {
             <Grid container
                 direction="column"
                 justify="center"
-                alignItems="center"
-                spacing={10}>
+                alignItems="center">
 
                 {/****** Home button ******/}
                 <Grid item className={classes.homeButtonContainer}>
@@ -240,10 +313,10 @@ export default function MinecraftServer({ data }) {
                 {/****** Examples ******/}
                 <Grid container
                     direction="row"
-                    justify="center"
-                    alignItems="center"
-                    spacing={6}
-                    className={classes.examplesContainer}>
+                    justify="space-evenly"
+                    alignItems="flex-start"
+                    className={classes.examplesContainer}
+                >
 
                     {/****** Twitch channel points example ******/}
                     <Grid item className={classes.videoContainer}>
@@ -309,6 +382,13 @@ export default function MinecraftServer({ data }) {
 
                 {/****** Achievements ******/}
                 <Grid item className={classes.darkBackground}>
+                    <div className={classes.achievementCurve}>
+                        <svg className={classes.achievementCurveSVG} viewBox="0 0 1200 120" preserveAspectRatio="none">
+                            <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" className={classes.curveFillGray}></path>
+                            <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" className={classes.curveFillGray}></path>
+                            <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" className={classes.curveFillGray}></path>
+                        </svg>
+                    </div>
                     <div className={classes.achievements}>
                         <p className={classes.achievementText}>
                             This Minecraft Server as well as a few small robots we created helped raise over
@@ -322,11 +402,18 @@ export default function MinecraftServer({ data }) {
                             <span className={classes.coloredAchievementText}> Australian Wildfires </span>
                         </p>
                     </div>
+                    <div className={classes.achievementCurveBottom}>
+                        <svg className={classes.achievementCurveSVGBottom} viewBox="0 0 1200 120" preserveAspectRatio="none">
+                            <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" className={classes.curveFillGray}></path>
+                            <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" className={classes.curveFillGray}></path>
+                            <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" className={classes.curveFillGray}></path>
+                        </svg>
+                    </div>
                 </Grid>
 
                 {/****** Examples Container ******/}
                 <Grid item className={classes.moreExamples}>
-                    <h1>More Examples</h1>
+                    <h1 className={classes.moreExamplesTitle}>More Examples</h1>
                     <Grid container
                         direction="row"
                         justify="center"
@@ -359,6 +446,13 @@ export default function MinecraftServer({ data }) {
                         </Grid>
 
                     </Grid>
+                    <div className={classes.examplesCurveBottom}>
+                        <svg className={classes.examplesCurveSVGBottom} viewBox="0 0 1200 120" preserveAspectRatio="none">
+                            <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" className={classes.curveFillRed}></path>
+                            <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" className={classes.curveFillRed}></path>
+                            <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" className={classes.curveFillRed}></path>
+                        </svg>
+                    </div>
                 </Grid>
 
                 {/****** More info ******/}
@@ -384,8 +478,15 @@ export default function MinecraftServer({ data }) {
                 </Grid>
 
                 {/****** Future Features ******/}
-                <Grid item>
-                    <div className={classes.featuresContainer}>
+                <Grid item className={classes.featuresContainer}>
+                    <div className={classes.achievementCurve}>
+                        <svg className={classes.achievementCurveSVG} viewBox="0 0 1200 120" preserveAspectRatio="none">
+                            <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" className={classes.curveFillRed}></path>
+                            <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" className={classes.curveFillRed}></path>
+                            <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" className={classes.curveFillRed}></path>
+                        </svg>
+                    </div>
+                    <div className={classes.featuresContainerDiv}>
                         <h3>Future Features:</h3>
                         <p>- Front End Website for purchasing similar to an online shop</p>
                         <p>- Front End Website for Mod control of redemptions</p>
@@ -396,6 +497,13 @@ export default function MinecraftServer({ data }) {
 
                 {/****** Footer ******/}
                 <Grid item className={classes.footerBackground}>
+                    <div className={classes.footerCurve}>
+                        <svg className={classes.footerCurveSVG} viewBox="0 0 1200 120" preserveAspectRatio="none">
+                            <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" className={classes.curveFillBlue}></path>
+                            <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" className={classes.curveFillBlue}></path>
+                            <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" className={classes.curveFillBlue}></path>
+                        </svg>
+                    </div>
                     <div className={classes.footerContainer}>
                         <h3>Next project:</h3>
                         <Link className={classes.link} to={{ pathname: '/personalwebsite' }}>
