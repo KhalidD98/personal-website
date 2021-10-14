90aa40733c203d013a029250bef7401329cf4d97
Khalid Dakak <khalid.dakak8@gmail.com>
2021-10-13 22:51:04 -0500

    Completed AMR

--- src/AccessMyResearch.js
+++ src/AccessMyResearch.js
@@ -5,6 +5,7 @@ import { Grid } from '@material-ui/core'
 import { Button } from '@material-ui/core'
 import { Link } from "react-router-dom"
 import { Typography } from '@mui/material';
+import mainExample from '../src/content/AMR examples/AMR Main Example.mp4'
 import jsonData from "../src/content/projects.json"
 import AMRCard from "./content/AMRCard"
 import conversationCollection from './content/AMR examples/conversationCollection.PNG'
@@ -45,7 +46,7 @@ const useStyles = makeStyles((theme) => ({
     },
     dependencyContainer: {
         marginBottom: '16vh',
-        marginTop: '16vh',
+        marginTop: '10vh',
         fontSize: '20px',
         [theme.breakpoints.down('xs')]: {
             fontSize: '15px',
@@ -82,7 +83,13 @@ const useStyles = makeStyles((theme) => ({
     databaseTitle: {
         marginTop: '2rem'
     },
-    goalContainer: {
+    lightBackground: {
+        backgroundColor: '#3ea369',
+    },
+    textContainer: {
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
         [theme.breakpoints.up('md')]: {
             width: '60%',
         },
@@ -91,21 +98,41 @@ const useStyles = makeStyles((theme) => ({
         },
     },
     roleContainer: {
-        [theme.breakpoints.up('md')]: {
-            width: '60%',
-        },
-        [theme.breakpoints.down('md')]: {
-            width: '80%',
-        },
+        marginTop: '1rem',
+        width: '100%',
+        backgroundColor: '#3ea369',
     },
     challengeContainer: {
-        [theme.breakpoints.up('md')]: {
-            width: '60%',
-        },
-        [theme.breakpoints.down('md')]: {
-            width: '80%',
-        },
-
+        width: '100%',
+        backgroundColor: '#3ea369',
+    },
+    mainExample: {
+        // padding: '0px',
+        // margin: '0px'
+    },
+    exampleVideoItem: {
+        width: '90%',
+        height: '90%',
+        // width: '1000px',
+        // height: '700px',
+        // [theme.breakpoints.down('sm')]: {
+        //     width: '300px',
+        //     height: '300px'
+        // },
+    },
+    shapeDivider: {
+        // position: 'absolute',
+        top: '0',
+        left: '0',
+        width: '100%',
+        overflow: 'hidden',
+        lineHeight: '0',
+    },
+    triangleSVG: {
+        position: 'relative',
+        display: 'block',
+        width: 'calc(100% + 1.3px)',
+        height: '70px',
     },
 }))
 
@@ -148,26 +175,45 @@ export default function AccessMyResearch({ data }) {
                     {jsonData[3].description}
                 </Grid>
 
-                {/*-----  Our Goal  -----*/}
-                <Grid item className={classes.goalContainer}>
-                    <h1>
-                        The Goal
-                    </h1>
-                    <p>
-                        The primary goal for my group was to implement a fully functional chat system feature both on the front end and back end of the website. We decided to use Firestore from Firebase as the cloud-hosted NoSQL database because it is easily scalable, has a multi-model structure, and has real-time document listening capabilities.
-                    </p>
-                    <p>
-                        On the contrary, relational databases store data in a fixed and predefined structure. This means you must design and create the database schema in terms of tables and columns when starting development. Anytime the requirements are modified, changes in the schema must be made, which includes defining new relations, creating new columns, and reflecting the changes in your application. NoSQL on the other hand provides more flexibility. There is no requirement to design a schema in order to begin working on the application, and it does not put a restriction on the types of data you can store together.
-                    </p>
-                    <p>
-                        The primary reason we chose a NoSQL database, like Firestore, is because it has the ability to scale easily. NoSQL databases are built on a masterless, peer-to-peer architecture, unlike relational databases. Data is partitioned and balanced across multiple nodes in a cluster, and aggregated queries are automatically distributed by default. This allows for easy scalability by simply executing a few commands which adds a new cluster to the server. This also improves the performance, allowing for continuous availability.
-
-                    </p>
-                    <p>
-                        Finally, Firestore has an onSnapshot() method  that allows you to listen to any document changes. When a new message is created, the method will run for every user within the conversation, rendering the new messages in real-time. This creates seamless conversations and eliminates waiting for messages to appear. Additional features can be implemented, such as "typing..." notifications which indicate if a user is in the middle of sending a message.
-
-                    </p>
+                {/* Main Example Video */}
+                <Grid item style={{ textAlign: "center" }} className={classes.mainExample}>
+                    <video className={classes.exampleVideoItem} playbackRate={3} preload="auto" autoPlay loop muted>
+                        <source src={mainExample} type="video/mp4"></source>
+                    </video>
                 </Grid>
+
+                {/*-----  Our Goal  -----*/}
+                <Grid item className={classes.lightBackground}>
+                    <div className={classes.shapeDivider}>
+                        <svg className={classes.triangleSVG} style={{ fill: '#358D5A' }} viewBox="0 0 1200 120" preserveAspectRatio="none">
+                            <path d="M1200 0L0 0 598.97 114.72 1200 0z"></path>
+                        </svg>
+                    </div>
+                    <div className={classes.textContainer}>
+                        <h1>
+                            The Goal
+                        </h1>
+                        <p>
+                            The primary goal for my group was to implement a fully functional chat system feature both on the front end and back end of the website. We decided to use Firestore from Firebase as the cloud-hosted NoSQL database because it is easily scalable, has a multi-model structure, and has real-time document listening capabilities.
+                        </p>
+                        <p>
+                            On the contrary, relational databases store data in a fixed and predefined structure. This means you must design and create the database schema in terms of tables and columns when starting development. Anytime the requirements are modified, changes in the schema must be made, which includes defining new relations, creating new columns, and reflecting the changes in your application. NoSQL on the other hand provides more flexibility. There is no requirement to design a schema in order to begin working on the application, and it does not put a restriction on the types of data you can store together.
+                        </p>
+                        <p>
+                            The primary reason we chose a NoSQL database, like Firestore, is because it has the ability to scale easily. NoSQL databases are built on a masterless, peer-to-peer architecture, unlike relational databases. Data is partitioned and balanced across multiple nodes in a cluster, and aggregated queries are automatically distributed by default. This allows for easy scalability by simply executing a few commands which adds a new cluster to the server. This also improves the performance, allowing for continuous availability.
+
+                        </p>
+                        <p>
+                            Finally, Firestore has an onSnapshot() method  that allows you to listen to any document changes. When a new message is created, the method will run for every user within the conversation, rendering the new messages in real-time. This creates seamless conversations and eliminates waiting for messages to appear. Additional features can be implemented, such as "typing..." notifications which indicate if a user is in the middle of sending a message.
+
+                        </p>
+                    </div>
+                </Grid>
+                <div className={classes.shapeDivider}>
+                    <svg style={{ fill: '#3EA369' }} className={classes.triangleSVG} viewBox="0 0 1200 120" preserveAspectRatio="none">
+                        <path d="M1200 0L0 0 598.97 114.72 1200 0z"></path>
+                    </svg>
+                </div>
 
                 {/*----- Database Structure Title -----*/}
                 <Grid item className={classes.databaseTitle}>
@@ -180,41 +226,62 @@ export default function AccessMyResearch({ data }) {
                     <Grid container
                         direction="row"
                         justifyContent="center"
-                        alignItems="center">
-                        <AMRCard
-                            src={conversationCollection}
-                            info={"We have a conversations collection that contains every conversation ever had"}
-                        />
-                        <AMRCard
-                            src={conversationMessages}
-                            info={"Within each conversation you have a messages collection, this contains every message from that individual conversation. As well as fields that contains every user within this conversation, allowing one-on-one chats, or group conversations."}
-                        />
-                        <AMRCard
-                            src={conversationMessage}
-                            info={"Within each messages field there is data containing the user who sent it, the message itself, as well as the timestamp allowing us to query them by timestamp"}
-                        />
+                        alignItems="center"
+                        style={{ justifyContent: 'center' }}
+                        spacing={2}>
+                        <Grid item>
+                            <AMRCard
+                                src={conversationCollection}
+                                info={"We have a conversations collection that contains every conversation ever had"}
+                            />
+                        </Grid>
+                        <Grid item >
+                            <AMRCard
+                                src={conversationMessages}
+                                info={"Within each conversation you have a messages collection, this contains every message from that individual conversation. As well as fields that contains every user within this conversation, allowing one-on-one chats, or group conversations."}
+                            />
+                        </Grid>
+                        <Grid item >
+                            <AMRCard
+                                src={conversationMessage}
+                                info={"Within each messages field there is data containing the user who sent it, the message itself, as well as the timestamp allowing us to query them by timestamp"}
+                            />
+                        </Grid>
                     </Grid>
                 </Grid>
 
-
                 {/*-----  My Role  -----*/}
                 <Grid item className={classes.roleContainer}>
-                    <h1>
-                        My Role
-                    </h1>
-                    <p>
-                        Our team had two members. I was the developer of all the functions that were used to communicate with the Firestore Database. The other team member used my functions to display information from the database on screen using the Vue.Js Framework. These functions were used to query information, such as usernames, past messages, and message timestamps from Firestore. This data was put into a stack to keep the order of messages intact. Each message was held in an object containing the user who sent it, the text, and the message timestamp. My team member then easily created a loop displaying each message in chronological order and would reformat the timestamp according to the instructions given to us by our team leads.
-                    </p>
+                    <div className={classes.shapeDivider}>
+                        <svg className={classes.triangleSVG} style={{ fill: '#358D5A' }} viewBox="0 0 1200 120" preserveAspectRatio="none">
+                            <path d="M1200 0L0 0 598.97 114.72 1200 0z"></path>
+                        </svg>
+                    </div>
+                    <div className={classes.textContainer}>
+                        <h1>
+                            My Role
+                        </h1>
+                        <p>
+                            Our team had two members. I was the developer of all the functions that were used to communicate with the Firestore Database. The other team member used my functions to display information from the database on screen using the Vue.Js Framework. These functions were used to query information, such as usernames, past messages, and message timestamps from Firestore. This data was put into a stack to keep the order of messages intact. Each message was held in an object containing the user who sent it, the text, and the message timestamp. My team member then easily created a loop displaying each message in chronological order and would reformat the timestamp according to the instructions given to us by our team leads.
+                        </p>
+                    </div>
                 </Grid>
                 {/*-----  The Challenges  -----*/}
                 <Grid item className={classes.challengeContainer}>
-                    <h1>
-                        Biggest Challenge
-                    </h1>
-                    <p>
-                        The problem we faced when creating this new feature was accessing AWS Cognito, which would allow us to retrieve the display name for the logged in user. My teammate and I had no prior experience with AWS Cognito, so we swiftly taught ourselves how it worked. Once I retrieved the display name, it was assigned to the users collection for each conversation document.
-                    </p>
+                    <div className={classes.textContainer}>
+                        <h1>
+                            Biggest Challenge
+                        </h1>
+                        <p>
+                            The problem we faced when creating this new feature was accessing AWS Cognito, which would allow us to retrieve the display name for the logged in user. My teammate and I had no prior experience with AWS Cognito, so we swiftly taught ourselves how it worked. Once I retrieved the display name, it was assigned to the users collection for each conversation document.
+                        </p>
+                    </div>
                 </Grid>
+                <div className={classes.shapeDivider}>
+                    <svg className={classes.triangleSVG} style={{ fill: '#3EA369' }} viewBox="0 0 1200 120" preserveAspectRatio="none">
+                        <path d="M1200 0L0 0 598.97 114.72 1200 0z"></path>
+                    </svg>
+                </div>
 
                 {/*----- Dependencies -----*/}
                 <Grid item className={classes.dependencyContainer}>
@@ -232,13 +299,11 @@ export default function AccessMyResearch({ data }) {
 
                 {/*----- Footer -----*/}
                 <Grid item className={classes.footerBackground}>
-                    {/* <div className={classes.footerCurve}>
-                        <svg className={classes.footerCurveSVG} viewBox="0 0 1200 120" preserveAspectRatio="none">
-                            <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" className={classes.curveFillBlue}></path>
-                            <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" className={classes.curveFillBlue}></path>
-                            <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" className={classes.curveFillBlue}></path>
+                    <div className={classes.shapeDivider}>
+                        <svg className={classes.triangleSVG} style={{ fill: '#358D5A' }} viewBox="0 0 1200 120" preserveAspectRatio="none">
+                            <path d="M1200 0L0 0 598.97 114.72 1200 0z"></path>
                         </svg>
-                    </div> */}
+                    </div>
                     <div className={classes.footerContainer}>
                         <h3>Next project:</h3>
                         <Link className={classes.link} to={{ pathname: '/personalwebsite' }}>
@@ -248,6 +313,6 @@ export default function AccessMyResearch({ data }) {
                 </Grid>
 
             </Grid>
-        </motion.div>
+        </motion.div >
     )
 }
